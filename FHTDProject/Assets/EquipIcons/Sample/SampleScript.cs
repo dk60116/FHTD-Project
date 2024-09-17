e31adfee7d0c510ee11a9e34ca82c825f1b53a94
한택근 <dk60116@gmail.com>
2024-09-18 01:08:01 +0900

    18

---
+++ FHTDProject/Assets/EquipIcons/Sample/SampleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleScript : MonoBehaviour
+{
+    public Transform IconPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 1; i <= 245; i++)
+        {
+            var go = Instantiate(IconPrefab, transform, false);
+            go.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Equip_" + i.ToString().PadLeft(3, '0'));
+        }
+    }
+}

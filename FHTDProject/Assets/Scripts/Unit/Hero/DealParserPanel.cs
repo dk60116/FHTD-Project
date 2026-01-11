8b9de6dca60a6c7cd590d0d1c5c89629fbdf553d
HTK <dk60116@gmail.com>
2026-01-11 18:11:50 +0900

    Fix DealParser

--- FHTDProject/Assets/Scripts/Unit/Hero/DealParserPanel.cs
+++ FHTDProject/Assets/Scripts/Unit/Hero/DealParserPanel.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -29,55 +28,109 @@ public class DealParserPanel : MonoBehaviour
     [SerializeField, ReadOnlyInspector]
     private List<RectTransform> listOrgYList;
 
+    private Coroutine smoothCoroutine;
+
+    private void Awake()
+    {
+        if (uiScroll == null || listElements == null || listElements.Count == 0)
+            Init();
+    }
+
     [ContextMenu("Init")]
     private void Init()
     {
         uiScroll = GetComponentInChildren<ScrollRect>();
 
         listElements = new List<DealParserElemnt>();
-
         listOrgYList = new List<RectTransform>();
+
+        if (uiScroll == null || uiScroll.content == null)
+            return;
 
         for (int i = 0; i < uiScroll.content.childCount; i++)
         {
             var elementTransform = uiScroll.content.GetChild(i);
-            listElements.Add(elementTransform.GetComponent<DealParserElemnt>());
-            listOrgYList.Add(elementTransform.GetComponent<RectTransform>());
+            if (elementTransform == null)
+                continue;
+
+            var element = elementTransform.GetComponent<DealParserElemnt>();
+            if (element != null)
+                listElements.Add(element);
+
+            var rect = elementTransform.GetComponent<RectTransform>();
+            if (rect != null)
+                listOrgYList.Add(rect);
         }
     }
 
     void Start()
     {
+        if (uiScroll == null || listElements == null || listElements.Count == 0)
+            Init();
+
         UpdateTotalDamage();
 
-        foreach (var item in listElements)
-            item.gameObject.SetActive(false);
+        if (listElements != null)
+        {
+            foreach (var item in listElements)
+            {
+                if (item != null)
+                    item.gameObject.SetActive(false);
+            }
+        }
 
-        if (InGameManager.instance.status == StageStatus.Running)
+        if (InGameManager.instance != null && InGameManager.instance.status == StageStatus.Running)
             UpdateHeroList();
     }
 
     private void OnEnable()
     {
-        StartCoroutine(SmoothUIUpdateCoroutine());
+        if (smoothCoroutine == null)
+            smoothCoroutine = StartCoroutine(SmoothUIUpdateCoroutine());
+    }
+
+    private void OnDisable()
+    {
+        if (smoothCoroutine != null)
+        {
+            StopCoroutine(smoothCoroutine);
+            smoothCoroutine = null;
+        }
     }
 
     public void OnOffPanel(bool _bOn)
     {
-        mainPanel.gameObject.SetActive(_bOn);
+        if (mainPanel != null)
+            mainPanel.gameObject.SetActive(_bOn);
     }
 
     IEnumerator SmoothUIUpdateCoroutine()
     {
         while (true)
         {
-            foreach (var item in listElements)
+            if (listElements != null)
             {
-                if (!item.gameObject.activeSelf)
-                    continue;
+                for (int i = 0; i < listElements.Count; i++)
+                {
+                    var item = listElements[i];
+                    if (item == null || !item.gameObject.activeSelf)
+                        continue;
 
-                item.display.position = Vector3.Lerp(item.display.position, listElements[item.rank].transform.position, 0.25f);
+                    if (item.display == null)
+                        continue;
+
+                    int targetRank = item.rank;
+                    if (targetRank < 0 || targetRank >= listElements.Count)
+                        continue;
+
+                    var target = listElements[targetRank];
+                    if (target == null)
+                        continue;
+
+                    item.display.position = Vector3.Lerp(item.display.position, target.transform.position, 0.25f);
+                }
             }
+
             yield return null;
         }
     }
@@ -87,11 +140,26 @@ public class DealParserPanel : MonoBehaviour
         iTotalDamage = 0;
         UpdateTotalDamage();
 
+        if (listElements == null || listElements.Count == 0)
+            return;
+
+        if (InGameManager.instance == null || InGameManager.instance.cPlayerController == null || InGameManager.instance.cPlayerController.placedHeros == null)
+        {
+            for (int i = 0; i < listElements.Count; i++)
+                listElements[i].gameObject.SetActive(false);
+            return;
+        }
+
         int _iActiveCount = 0;
 
-        List<Hero> _listHero = InGameManager.instance.cPlayerController.placedHeros.OrderByDescending(x => x.heroCost).OrderByDescending(x => x.unitLevel).ToList();
+        List<Hero> _listHero = InGameManager.instance.cPlayerController.placedHeros
+            .OrderByDescending(x => x.unitLevel)
+            .ThenByDescending(x => x.heroCost)
+            .ToList();
 
-        for (int i = 0; i < _listHero.Count; i++)
+        int count = Mathf.Min(_listHero.Count, listElements.Count);
+
+        for (int i = 0; i < count; i++)
         {
             listElements[i].InitHero(_listHero[i]);
             listElements[i].gameObject.SetActive(true);
@@ -100,9 +168,7 @@ public class DealParserPanel : MonoBehaviour
         }
 
         for (int i = _iActiveCount; i < listElements.Count; i++)
-        {
             listElements[i].gameObject.SetActive(false);
-        }
     }
 
     public void AddTotalDamage(int _iDamage)
@@ -113,21 +179,41 @@ public class DealParserPanel : MonoBehaviour
 
     private void UpdateTotalDamage()
     {
-        txtTotalDmg.text = iTotalDamage.ToString();
+        if (txtTotalDmg != null)
+            txtTotalDmg.text = iTotalDamage.ToString();
     }
 
     public Sprite GetHeroFrameSprite(int _iLevel)
     {
-        return listHeroFrame[_iLevel - 1];
+        if (listHeroFrame == null || listHeroFrame.Count == 0)
+            return null;
+
+        int idx = Mathf.Clamp(_iLevel - 1, 0, listHeroFrame.Count - 1);
+        return listHeroFrame[idx];
     }
 
     public void UpdateAllUnit()
     {
+        if (listElements == null || listElements.Count == 0)
+        {
+            iFirstDmg = 0;
+            return;
+        }
+
         List<DealParserElemnt> _listTemp = new List<DealParserElemnt>();
+
         foreach (var item in listElements)
         {
-            if (!item.gameObject.activeSelf) continue;
+            if (item == null || !item.gameObject.activeSelf)
+                continue;
+
             _listTemp.Add(item);
+        }
+
+        if (_listTemp.Count == 0)
+        {
+            iFirstDmg = 0;
+            return;
         }
 
         _listTemp.Sort((x, y) => y.totalDamage.CompareTo(x.totalDamage));
@@ -144,4 +230,3 @@ public class DealParserPanel : MonoBehaviour
     public int totalDamage { get { return iTotalDamage; } }
     public int firstDamage { get { return iFirstDmg; } }
 }
-
